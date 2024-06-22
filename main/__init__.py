a142ae325661a1d022676b7af27ee26c12415897
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2024-06-22 23:10:20 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -19,15 +19,15 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 uvloop.install()
 MDB = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn"
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = "21266744" #config("API_ID", default=None, cast=int)
+API_HASH = "41028624362faeb4515884b4fe67a9b9" #config("API_HASH", default=None)
+BOT_TOKEN = "7310794519:AAFFiZVUnMVQn2frTDKk61rzyiJHPAYDXoA" #config("BOT_TOKEN", default=None)
+SESSION = "BQFEgTgAuKzuY_nfSFx-2fw1zLa83dr78jcwjInzzrJuxHytgqjAKVkr2tykenkv_VIWQWW-FqqjU8ip7S2QNywX8ZmvtWbzIrfxrbwAal6oj06ayR5nm1-OQA4StOv5tTOYI28vn8QCjZLajVaaPxNMQixgfzQNcnCqsVrspxta9Dw1S5uzjU1DDRHHo5Gz0adEU4p9QjBcYR5W_vzQQe_3V7s45mSrsG_HWKSUjFXg1UP3iWFBvJKQlNQoFphG8xYaSGLU6rcPk65g7oQ-rP8dqII6T6QM1CNyTDw9suXniuxdAnrjq2zJgux0NHq_dBuZmpjr_BLKBhj3qFcYRSNICsOEoQAAAABjoCK0AA" #config("SESSION", default=None)
+FORCESUB = "samrabotss" #config("FORCESUB", default=None)
+AUTH = "6756781098" #config("AUTH", default=None)
 MONGODB = config("MONGODB", default=MDB)
-OWN = 7065117445 # edit this
-GROUP = -1001878947221 # edit this
+OWN = 6756781098 # edit this
+GROUP = -1002240648361 # edit this
 OWNER_ID = int(config("OWNER_ID", default=OWN))
 LOG_GROUP = int(config("LOG_GROUP", default=GROUP))
 
@@ -44,7 +44,7 @@ userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 try:
     userbot.start()
 except BaseException:
-    print("Your session expired please re add that... thanks @dev_gagan.")
+    print("Your session expired please re add that... thanks.")
     sys.exit(1)
 
 Bot = Client(
